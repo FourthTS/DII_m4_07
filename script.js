ccc397bdbd37062de8878296caee83c7c2fef66d
FourthTs <fourth4884@gmail.com>
2022-05-17 14:50:22 +0700

    fibo

--- script.js
+++ script.js
@@ -1,58 +1,10 @@
-var x=5
-{
-    let x =1
+let numB=0;
+let numA=1;
+let num=10
+let sum=1;
+for(let i=1;i<num;i++){
+    numA=numB;
+    numB=sum;
+    sum=numA+numB;
 }
-/*console.log(x+x)
-let price = 100
-let vat =70
-console.log(price * (vat/100));
-*/
-/*let money=350000; 
-let tax='free bro';
-if(money>=150000){
-
-    tax=(money-150000)*(0.05);
-
-}else if(money>300000){
-
-    tax=((money-300000)*(0.1))+7500;
-
-}else if(money>500000){
-
-    tax=((money-500000)*(0.15))+27500;
-
-}else if(money>750000){
-    
-    tax=((money-750000)*(0.2))+65000;
-
-}else if(money>1000000){
-
-    tax=((money-1000000)*(0.25))+115000;
-
-}
-else if(money>2000000){
-
-    tax=((money-2000000)*(0.3))+365000;
-
-}
-else if(money>5000000){
-
-    tax=((money-5000000)*(0.35))+1265000;
-    
-}else{
-    'free bro';
-}*/
-let number = 5;
-let sum = 1;
-for(let i=1; i<= number; i++){
-    sum *= i; 
-}
-console.log(sum);
-
-n=5;
-let sum_while=1;
-while(n>0){
-    sum_while  *= n;
-    n--;
-}
-console.log(sum_while);
+console.log(sum)
