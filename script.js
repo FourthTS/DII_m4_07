0a11a2ee19b0645a2cc8a2cf0e3a734a2f3f57bf
FourthTs <fourth4884@gmail.com>
2022-05-18 10:57:51 +0700

    edit variable

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-let cmu = {     
+let place=[3]
+
+place[0] = {     
     'name': 'chaing mai university',     
     'lat': 18.8046735,     
     'long': 98.9528446, 
 } 
-let maya = {     
+place[1] = {     
     'name': 'maya',     
     'lat': 18.8047037,     
     'long': 98.9440898, 
 } 
-let cenfest = {     
+place[2] = {     
     'name': 'central chaing mai',     
     'lat': 18.8048018,     
     'long': 98.917825, 
 } 
-
-let place=[cmu,maya,cenfest]
-
-console.log(place[1]);
+console.log(place[1].name);
