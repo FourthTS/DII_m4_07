861e4e04a6e847ddc8a77dd8521770ba4dbe6e1f
FourthTs <fourth4884@gmail.com>
2022-05-18 12:23:56 +0700

    ok and cancel button

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function alert1() {
   alert("let me see ...");
 }
+function alert2() {
+    alert("Hmmmmm you need to cancel");
+  }
 let ok = document.getElementById("ok");
 ok.addEventListener("click", alert1);
+let cencel = document.getElementById("cancel");
+cancel.addEventListener("click", alert2);
+
+
+let okButton = document.getElementById("ok");
+okButton.addEventListener('click',function (e){
+    alert("2+2=4")
+
+})
