723b56e48da2afb89936bdadc24cab7b9053a284
FourthTs <fourth4884@gmail.com>
2022-05-18 12:30:46 +0700

    add new button and dbl click

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-function alert1() {
-  alert("let me see ...");
-}
-function alert2() {
-    alert("Hmmmmm you need to cancel");
-  }
-let ok = document.getElementById("ok");
-ok.addEventListener("click", alert1);
-let cencel = document.getElementById("cancel");
-cancel.addEventListener("click", alert2);
-
-
-let okButton = document.getElementById("ok");
-okButton.addEventListener('click',function (e){
-    alert("2+2=4")
-
-})
+let ok = document.getElementById('ok'); 
+let cancel = document.getElementById('cancel'); 
+let showDate = document.getElementById('showDate'); 
+ok.addEventListener('click', () => alert('let me see ...')); 
+cancel.addEventListener('click', () => alert('not ok ...')); 
+showDate.addEventListener('click', function(e) {     
+    document.getElementById('emptySpace').innerText = Date(); 
+});
+showDate.addEventListener('dblclick', function(e) {     
+    document.getElementById('emptySpace').innerText=" " 
+});
