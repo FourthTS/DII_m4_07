2589da5af02d01ece3cc36f2fd131d1c2f473e87
FourthTs <fourth4884@gmail.com>
2022-05-18 10:54:28 +0700

    creat array

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-let numB=0;
-let numA=1;
-let num=10
-let sum=1;
-for(let i=1;i<num;i++){
-    numA=numB;
-    numB=sum;
-    sum=numA+numB;
-}
-console.log(sum)
+let cmu = {     
+    'name': 'chaing mai university',     
+    'lat': 18.8046735,     
+    'long': 98.9528446, 
+} 
+let maya = {     
+    'name': 'maya',     
+    'lat': 18.8047037,     
+    'long': 98.9440898, 
+} 
+let cenfest = {     
+    'name': 'central chaing mai',     
+    'lat': 18.8048018,     
+    'long': 98.917825, 
+} 
+
+let place=[cmu,maya,cenfest]
+
+console.log(place[1]);
